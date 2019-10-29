8db4d2c8ebb66289b295715e866cadf5eed126d7
Sullivan Pierce <sullypierce@gmail.com>
2019-10-29 14:55:02 -0500

    lightning exercises done

---
+++ src/scripts/main.js
@@ -0,0 +1,19 @@
+//lightning exercise one
+
+const docObj (name, specialty, practiceAddress) => {
+    return {
+        name: name,
+        specialty: specialty,
+        address: practiceAddress
+    };
+}
+
+
+//lightning exercise 2
+
+const pet (name, breed) {
+    return {
+        name: name,
+        breed: breed
+    };
+}
